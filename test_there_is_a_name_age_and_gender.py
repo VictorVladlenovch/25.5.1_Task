f379da0e4f3a36c80ebba98e50130b90fd776b5f
VictorVladlenovch <121546893+VictorVladlenovch@users.noreply.github.com>
2023-03-02 11:12:33 +0300

    Update test_there_is_a_name_age_and_gender.py

--- test_there_is_a_name_age_and_gender.py
+++ test_there_is_a_name_age_and_gender.py
@@ -4,14 +4,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 def test_there_is_a_name_age_and_gender(go_to_my_pets):
-   '''Поверяем что на странице со списком моих питомцев, у всех питомцев есть имя, возраст и порода'''
+   '''Поверяем, что на странице со списком моих питомцев, у всех питомцев есть имя, возраст и порода'''
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
    # Сохраняем в переменную pet_data элементы с данными о питомцах
    pet_data = pytest.driver.find_elements(By.CSS_SELECTOR, '.table.table-hover tbody tr')
 
    # Настраиваем переменную явного ожидания:
-   wait = WebDriverWait(pytest.driver, 5)
+   wait = WebDriverWait(pytest.driver, 10)
 
    # Ожидаем, что данные всех питомцев, найденных локатором css_locator = '.table.table-hover tbody tr', видны на странице:
    for i in range(len(pet_data)):
